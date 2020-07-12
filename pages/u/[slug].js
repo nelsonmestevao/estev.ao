f36c0a990d0d3d9e430e06b671e8120738f3f8c9
Nelson Estevão <nelsonmestevao@gmail.com>
2020-07-12 13:43:48 +0100

    Improve error handler and security issues

--- pages/u/[slug].js
+++ pages/u/[slug].js
@@ -4,10 +4,10 @@ import Error from '../../components/Error';
 
 import 'isomorphic-fetch';
 
-export default function Slug() {
+export default function Slug({ code, message }) {
   return (
     <Layout>
-      <Error code={404} description="Slug not found" />
+      <Error code={code} description={message} />
     </Layout>
   );
 }
@@ -16,7 +16,12 @@ Slug.getInitialProps = async ({ res, query }) => {
   const { slug } = query;
   const { url } = await (
     await fetch(`${process.env.NEXT_PUBLIC_APP_DOMAIN}/api/links/${slug}`)
-  ).json();
+  )
+    .json()
+    .catch((error) => {
+      console.error(error);
+      return { code: 500, message: "I don't know what happened" };
+    });
 
   if (url) {
     res.writeHead(301, {
@@ -25,5 +30,5 @@ Slug.getInitialProps = async ({ res, query }) => {
     res.end();
   }
 
-  return {};
+  return { code: 404, message: 'Slug not found' };
 };
