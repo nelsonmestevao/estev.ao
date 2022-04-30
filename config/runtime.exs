479e488d207a59bb4a318f1da5782986f16e8431
Nelson Estevão <nelsonmestevao@gmail.com>
2022-04-30 16:56:44 +0100

    Fix url generation on prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -60,7 +60,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :estevao, EstevaoWeb.Endpoint,
-    url: [host: host, port: 443],
+    url: [host: host],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
