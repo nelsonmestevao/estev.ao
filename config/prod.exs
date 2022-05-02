bd7dee2aa5d52a18165d8793247c015f201d8792
Nelson Estevão <nelsonmestevao@gmail.com>
2022-05-02 14:49:24 +0100

    Update scheme url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :estevao, EstevaoWeb.Endpoint,
-  url: [host: "estev.ao", port: 80],
+  url: [scheme: "https", host: "estev.ao", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- config/runtime.exs
+++ config/runtime.exs
@@ -60,7 +60,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :estevao, EstevaoWeb.Endpoint,
-    url: [host: host],
+    url: [scheme: "https", host: host],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
