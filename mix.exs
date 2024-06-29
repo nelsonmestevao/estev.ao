2cc176d9e49efd481c9d1933cecb2a56d549376f
Nelson Estevão <nelsonmestevao@proton.me>
2024-06-29 23:06:40 +0100

    feat: reorder tool dependencies

--- mix.exs
+++ mix.exs
@@ -92,11 +92,11 @@ defmodule Estevao.MixProject do
       {:floki, ">= 0.30.0", only: :test},
 
       # tools
-      {:styler, "~> 0.7", only: [:dev, :test], runtime: false},
-      {:tailwind_formatter, "~> 0.4.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.30", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false},
+      {:styler, "~> 0.7", only: [:dev, :test], runtime: false},
+      {:tailwind_formatter, "~> 0.4.0", only: [:dev, :test], runtime: false}
     ]
   end
 
