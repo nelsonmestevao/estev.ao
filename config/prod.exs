237945db79d4bfb84bbe68cffc8c50dabfa4baf1
Nelson Estevão <nelsonmestevao@proton.me>
2025-05-22 12:25:10 +0100

    chore: clean up configuration

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,8 @@ import Config
 # before starting your production server.
 config :estevao, EstevaoWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto], exclude: ~r{^/health}]
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: false
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Estevao.Finch
