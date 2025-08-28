ae309feb7f40746fc9ca4ed3103d89aaa9a44512
Nelson Estevão <nelsonmestevao@proton.me>
2025-08-28 23:33:19 +0100

    chore: update flake setup

--- flake.nix
+++ flake.nix
@@ -2,25 +2,37 @@
   description = "Development Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    beam-flakes = {
+      url = "github:mhanberg/nix-beam-flakes";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = false;
-        };
-      in {
-        devShells.default = pkgs.mkShell {
-          name = "estevao";
+  outputs = inputs @ {
+    beam-flakes,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [beam-flakes.flakeModule];
 
-          buildInputs = with pkgs; [
-            beam28Packages.elixir_1_19
-          ];
+      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
+
+      perSystem = _: {
+        beamWorkspace = {
+          enable = true;
+          # devShell = {
+          #   languageServers.elixir = true;
+          #   languageServers.erlang = false;
+          #   phoenix = true;
+          # };
+          versions = {
+            fromToolVersions = ./.tool-versions;
+          };
         };
-      }
-    );
+      };
+    };
 }
